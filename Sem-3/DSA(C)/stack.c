df85e6b03486db7c34ee70f0ec676af559d38b9b
ツ『Love』ツ <lovekhatri9211@gmail.com>
2024-02-21 10:06:07 +0530

    Moved 3rd sem codes to Sem-3 folder

--- Sem-3/DSA(C)/stack.c
+++ Sem-3/DSA(C)/stack.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
-#include <conio.h>
 #define size 10
 
-void main()
+void push(int stack[], int *TOP, int a);
+void pop(int stack[], int *TOP);
+void display(int stack[], int *TOP);
+
+int main()
 {
     int stack[size];
     int pointer = -1;
     int *TOP = &pointer;
-    void push(int stack[], int a, int *TOP);
-    void pop(int stack[], int *TOP);
-    void display(int stack[], int *TOP);
-    push(stack, 10, TOP);
-    push(stack, 20, TOP);
-    push(stack, 30, TOP);
+
+    push(stack, TOP, 10);
+    push(stack, TOP, 20);
+    push(stack, TOP, 30);
     display(stack, TOP);
     pop(stack, TOP);
     display(stack, TOP);
+
+    return 0;
 }
-void push(int stack[], int a, int *TOP)
+
+void push(int stack[], int *TOP, int a)
 {
     if (*TOP == size - 1)
     {
@@ -29,6 +33,7 @@ void push(int stack[], int a, int *TOP)
         stack[*TOP] = a;
     }
 }
+
 void pop(int stack[], int *TOP)
 {
     if (*TOP == -1)
@@ -41,6 +46,7 @@ void pop(int stack[], int *TOP)
         *TOP = *TOP - 1;
     }
 }
+
 void display(int stack[], int *TOP)
 {
     for (int i = *TOP; i >= 0; i--)
