84d2dcdc6cb775277d0c8fb7f08e69dce1b16bd5
ツ『Love』ツ <lovekhatri9211@gmail.com>
2024-04-25 14:10:16 +0530

    Update: Linear Regression

--- Sem-4/ML/Prat_6_simpleRegression.py
+++ Sem-4/ML/Prat_6_simpleRegression.py
@@ -1,31 +1,47 @@
-# implementation of simple regression model for a real life problem using python
+import seaborn as sns
+iris = sns.load_dataset('iris')
 
-# Importing the libraries  
-import numpy as np
+X = iris['petal_length']
+y = iris['petal_width']
+
 import matplotlib.pyplot as plt
-import pandas as pd
+plt.scatter(X, y)
+plt.xlabel("petal length")
+plt.ylabel("petal width")
 
-# Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
-
-# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 23)
 
-# Fitting Simple Linear Regression to the Training set
+import numpy as np
+X_train = np.array(X_train).reshape(-1, 1)
+
+X_test = np.array(X_test).reshape(-1, 1)
+
 from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
 
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
+lr = LinearRegression()
 
-# Visualising the Training set results
-plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
+lr.fit(X_train, y_train)
+
+c = lr.intercept_
+
+m = lr.coef_
+
+Y_pred_train = m*X_train + c
+Y_pred_train.flatten()
+
+y_pred_train1 = lr.predict(X_train)
+
+import matplotlib.pyplot as plt
+plt.scatter(X_train, y_train)
+plt.plot(X_train, y_pred_train1, color ='red')
+plt.xlabel("petal length")
+plt.ylabel("petal width")
+
+y_pred_test1 = lr.predict(X_test)
+
+import matplotlib.pyplot as plt
+plt.scatter(X_test, y_test)
+plt.plot(X_test, y_pred_test1, color ='red')
+plt.xlabel("petal length")
+plt.ylabel("petal width")
